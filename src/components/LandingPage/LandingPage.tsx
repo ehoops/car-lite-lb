ba1af57a21d1c22861777a88e9de078a43d051b8
ehoops-cz <erin.hoops+gh@chanzuckerberg.com>
2023-02-26 15:21:00 -0800

    Appease linter

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,5 @@
 import { StyledWrapper } from "./landing_page_style";
 
-interface LandingPageProps {
-  // define types here
-}
-
-export const LandingPage = ({}: LandingPageProps) => {
-  return (
-        <StyledWrapper>Your new component</StyledWrapper>
-  );
+export const LandingPage = () => {
+  return <StyledWrapper>Your new component</StyledWrapper>;
 };
